5bb61997a64c3d9b4d261a5b92a19389e24b1679
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-22 10:36:50 +0200

    Main methods add

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 import festmeny.*;
 
 public class Main {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in); 
         String fajlNev = "festmenyek.csv";
         try {
             festmeny festmenye = new festmeny(fajlNev);
@@ -13,6 +15,30 @@ public class Main {
             System.err.printf("Hiba miatt nem található az %s fájl\n", fajlNev);
         }catch(IOException e){
             System.err.println("Ismeretlen hiba történt a fájl beolvasása során");
+        }
+        int i = 0;
+        int hanyadik = 0;
+        double menyi = 0;
+        String menu = "";
+        while(i<1){
+            System.out.println("Mit szeterne csinálni a(licit 10%) b(licit x%) c(x festmény adatai) d(exit)");
+            menu = sr.next();
+            if(menu.equals("d")){
+                i++;
+            }else if(menu.equals("a")){
+                System.out.print("Hanyadik festményre:");
+                hanyadik = sr.nextInt();
+            }else if(menu.equals("b")){
+                System.out.print("Hanyadik festményre:");
+                hanyadik = sr.nextInt();
+                System.out.print("Menyivel:");
+                menyi = sr.nextDouble();
+            }else if(menu.equals("c")){
+                System.out.print("Hanyadik festmény:");
+                hanyadik = sr.nextInt();
+            }else{
+                System.out.println("Hibaaaaaaaaaaaaaaaaaaaaaaaa! Próbáld újra!");
+            }           
         }
     }
 }
--- src/festmeny/festmeny.java
+++ src/festmeny/festmeny.java
@@ -2,6 +2,7 @@ package festmeny;
 
 import java.util.ArrayList;
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -11,22 +12,21 @@ public class festmeny {
     private String festo;
     private String stílus;   
     private int licitekSzama;
+    private double licitMeret;
     private LocalDateTime legutsoLicit;
     private boolean elkelt;
-
     private ArrayList festmenyek;
 
     public festmeny() {
         this.cím = "";
         this.festo = "";
         this.stílus = "";
-        this.licitekSzama = 0;
+        this.licitekSzama = 0;    
         this.legutsoLicit = null;
         this.elkelt = false;
     }
 
-    public festmeny(String cím, String festo, String stílus, int licitekSzama, LocalDateTime legutsoLicit,
-            boolean elkelt) {
+    public festmeny(String cím, String festo, String stílus, int licitekSzama, LocalDateTime legutsoLicit,boolean elkelt) {
         this.cím = cím;
         this.festo = festo;
         this.stílus = stílus;
@@ -48,5 +48,39 @@ public class festmeny {
         }
         br.close();
         fr.close();
+    }
+    public static void licit(boolean elkelt, int licitekSzama, double licitMeret, LocalDateTime legutsoLicit){
+        if(elkelt==true){
+            System.out.println("Ez a festmény már elkelt!");
+        }else if(licitekSzama  == 0){
+            licitMeret = 100;
+            licitekSzama++;
+            legutsoLicit = LocalDateTime.now();
+        }else{
+            licitMeret = licitMeret*1.1;
+            licitekSzama++;
+            legutsoLicit = LocalDateTime.now();
+        }
+    }
+    /*public static void licitmeret(boolean elkelt, int licitekSzama, double licitMeret, LocalDateTime legutsoLicit){
+        if(elkelt==true){
+            System.out.println("Ez a festmény már elkelt!");
+        }else if(licitekSzama  == 0){
+            licitMeret = 100;
+            licitekSzama++;
+            legutsoLicit = LocalDateTime.now();
+        }else if(menyi > 100 || menyi < 0){
+            System.out.println("Nem megfelelő méret csak (0-100) elfogadható!");
+        }else{
+            licitMeret = licitMeret*1.1;
+            licitekSzama++;
+            legutsoLicit = LocalDateTime.now();
+        }
+    }*/
+    @Override
+    public String toString() {
+        String elk = "";
+        if(elkelt==true){elk = "elkelt";}else{elk = "nem kelt el";}
+        return String.format("%s: %s(%s)\n %s \n %.0f - "+legutsoLicit+" (Összesen: %d db)", festo,cím,stílus,elk,licitMeret,licitekSzama);
     }
 }
